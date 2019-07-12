c645e496d7e200bf000a872fd2587c61120ce62a
alireza <alirezafa@rocketmail.com>
2019-07-12 23:00:55 +0430

    :sparkles: Improved Widget.

--- app/src/main/java/ir/sharif/taxifinder/widgets/TaxiWidgetService.kt
+++ app/src/main/java/ir/sharif/taxifinder/widgets/TaxiWidgetService.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
+import ir.sharif.taxifinder.FormatHelper
 import ir.sharif.taxifinder.R
 import ir.sharif.taxifinder.webservice.WebserviceHelper
 import ir.sharif.taxifinder.webservice.webservices.drivers.Driver
@@ -55,8 +56,11 @@ class TaxiRemoteViewsFactory(
     }
 
     override fun getViewAt(position: Int): RemoteViews {
+        val driver = widgetItems[position]
         return RemoteViews(context.packageName, R.layout.widget_item).apply {
-            setTextViewText(R.id.driver_name, widgetItems[position].firstName + " " + widgetItems[position].lastName)
+            setTextViewText(R.id.widget_fullName, driver.firstName + " " + driver.lastName)
+            setTextViewText(R.id.widget_phoneNumber, FormatHelper.toPersianNumber(driver.msisdn))
+            setTextViewText(R.id.widget_brandName, driver.carBrand)
         }
     }
 
